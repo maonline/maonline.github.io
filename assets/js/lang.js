3e38834754a29cf2288d0856772dc08b5f0bb94c
naz_7z <naz7kaed@gmail.com>
2019-02-09 12:15:40 +0900

    v1.4.1

--- assets/js/lang.js
+++ assets/js/lang.js
@@ -2,54 +2,54 @@
 let Language, langdata;
 
 $(window).on("load", function(){
-    if($.cookie('lang') == undefined) {
-        Language = $("language").attr("initial");
-    } else {
-        Language = $.cookie('lang', {path: '/' });
-    }
+	if($.cookie('lang') == undefined) {
+		Language = $("language").attr("initial");
+	} else {
+		Language = $.cookie('lang', {path: '/' });
+	}
 
-    Language = $("language").attr("initial");
-    langdata = $("language").attr("lang").split(",");
-    langmain(200);
+	Language = $("language").attr("initial");
+	langdata = $("language").attr("lang").split(",");
+	langmain(200);
 });
 
 function langmain(time){
-    this.main = function(){
-        url = location.href;
-        if(url.match("#ja") || url.match("#en")) {
+	this.main = function(){
+		url = location.href;
+		if(url.match("#ja") || url.match("#en")) {
             // console.log("yes!");
             for(i in langdata){
-                if(langdata[i].indexOf(url.split("#")[1]) >= 0){
-                    Language = url.split("#")[1];
-                    $.cookie('lang', Language, {path: '/' });
-                    $(".lang").each(function(i) {
-                        lang = $(this).attr("language");
-                        if(lang == Language) {
-                            $(this).show();
-                        } else {
-                            $(this).hide();
-                        }
-                    });
-                }
+            	if(langdata[i].indexOf(url.split("#")[1]) >= 0){
+            		Language = url.split("#")[1];
+            		$.cookie('lang', Language, {path: '/' });
+            		$(".lang").each(function(i) {
+            			lang = $(this).attr("language");
+            			if(lang == Language) {
+            				$(this).show();
+            			} else {
+            				$(this).hide();
+            			}
+            		});
+            	}
             }
         } else {
             // console.log("no!");
             $(".lang").each(function(i) {
-                $.cookie('lang', Language, {path: '/' });
-                lang = $(this).attr("language");
-                if(lang == Language) {
-                    $(this).show();
-                } else {
-                    $(this).hide();
-                }
+            	$.cookie('lang', Language, {path: '/' });
+            	lang = $(this).attr("language");
+            	if(lang == Language) {
+            		$(this).show();
+            	} else {
+            		$(this).hide();
+            	}
             });
         }
     }
     if (document.getElementById("cp_toggle02").checked == true) {
-        document.getElementById("cp_toggle02").checked = false;
+    	document.getElementById("cp_toggle02").checked = false;
     }
     setTimeout(this.main, time);
 }
 $(".language").on("click", function(){
-    langmain(200);
+	langmain(200);
 });
