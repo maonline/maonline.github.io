813ad1efea40d5cbed3ad8384b47dd0a5d0caa91
naz_7z <naz7kaed@gmail.com>
2019-03-21 21:04:31 +0900

    v1.8.3

--- assets/js/header.js
+++ assets/js/header.js
@@ -1,9 +1,13 @@
+var canRun = true;
+
 movefun = function( event ){
 	event.preventDefault();
 }
 
 $(function () {
 	$('._drawer_button').click(function () {
+		if (canRun == false) return;
+		canRun = false;
 
 		if ($('body').css('overflow') == 'hidden') {
 			$('body').css('overflow', 'auto');
@@ -23,16 +27,47 @@ $(function () {
 			$(this).removeClass('active');
 			$('._drawer_bg').fadeOut();
 			$('nav').removeClass('open');
-			$('.header-drawer').css('height', '100px');
+			sleep(50, function () {
+				$('.header-drawer').css('height', '100px');
+			});
 		}
+
+		canRun = true;
 	})
 
 	$('._drawer_bg').click(function () {
+		if (canRun == false) return;
+		canRun = false
 		$('body').css('overflow', 'auto');
 		// window.removeEventListener( 'touchmove' , movefun, { passive: false } );
 		$(this).fadeOut();
 		$('._drawer_button').removeClass('active');
 		$('nav').removeClass('open');
-		$('.header-drawer').css('height', '100px');
+		sleep(50, function () {
+			$('.header-drawer').css('height', '100px');
+		});
+		canRun = true;
 	});
 })
+
+function sleep(waitSec, callbackFunc) {
+	
+    // 経過時間（秒）
+    var spanedSec = 0;
+    
+    // 1秒間隔で無名関数を実行
+    var id = setInterval(function () {
+    	
+    	spanedSec++;
+    	
+        // 経過時間 >= 待機時間の場合、待機終了。
+        if (spanedSec >= waitSec) {
+        	
+            // タイマー停止
+            clearInterval(id);
+            
+            // 完了時、コールバック関数を実行
+            if (callbackFunc) callbackFunc();
+        }
+    }, 1);
+}
