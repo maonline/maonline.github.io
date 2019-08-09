2ea2d764c3bf1f75ff78f0c473df000c5c082e6e
naz_7z <naz7kaed@gmail.com>
2019-08-10 00:58:12 +0900

    v1.20.0

--- assets/js/count.js
+++ assets/js/count.js
@@ -17,8 +17,8 @@ function countDown() {
 			setTimeout('countDown()', 10);
 		}
 	} else {
-		openModal("https://www.youtube.com/embed/phkWBWyPafU");
-		document.getElementById("bg").style.backgroundImage = "url(./assets/image/bg/bg_14.jpg)";
-		document.getElementById("countdown").innerText = 'MAO SUMMER START';
+		// openModal("https://www.youtube.com/embed/phkWBWyPafU");
+		// document.getElementById("bg").style.backgroundImage = "url(./assets/image/bg/bg_14.jpg)";
+		// document.getElementById("countdown").innerText = 'MAO SUMMER START';
 	}
 }
